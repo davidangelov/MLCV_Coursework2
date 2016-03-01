792b532a0eb3d37ebe76e638f312f881ab59aabf
David Angelov <davidangelovbg@gmail.com>
2016-03-01 23:08:09 +0000

    Q1 - DONE
    David

--- Q1.m
+++ Q1.m
@@ -1,5 +1,8 @@
-A = imread('blackandyellow.png');
-cluster_num = 8;
+clear;
+close all;
+
+A = imread('cat.png');
+cluster_num = 2;
 
 red = A(:,:,1);
 green = A(:,:,2);
@@ -13,13 +16,16 @@ blue_vector = reshape(blue, num_element, 1);
 RGB_vector = double([red_vector, green_vector, blue_vector]);
 
 figure(1);
-scatter3(red_vector, green_vector, blue_vector, '.');
+imshow(A);
+title('Original Image');
 
 figure(2);
-imshow(A);
+scatter3(red_vector, green_vector, blue_vector, '.');
+title 'Data Points Representing Pixels in a RGB Image'
 
 rng(1)
 
+Path_array = zeros(1,cluster_num*3);
 [~,C] = kmeans(RGB_vector,cluster_num,'MaxIter', 1, 'Start', 'plus');
 iteration_count = 1;
 
@@ -42,16 +48,44 @@ hold on
         scatter3(RGB_vector(idx==i,1),RGB_vector(idx==i,2),RGB_vector(idx==i,3),'.', 'MarkerFaceColor',rand(1,3));
     end;
 end;
+title 'Cluster Assignment'
 hold off
 
 figure(4);
+title 'Cluster Centroids Iteration and Convergence'
 hold on
 for i = 0:cluster_num-1
     scatter3(Path_array(:,1+i*3),Path_array(:,2+i*3),Path_array(:,3+i*3),'.')
-    %line(Path_array(:,1+i*3),Path_array(:,2+i*3),Path_array(:,3+i*3))
+    line(Path_array(:,1+i*3),Path_array(:,2+i*3),Path_array(:,3+i*3))
 end
 plot3(Path_array(:,1+i*3),Path_array(:,2+i*3),Path_array(:,3+i*3))
 scatter3(C(:,1),C(:,2),C(:,3),'kx')
 hold off
 
+%Quantisation
+image_size = size(A);
+
+for i = 1:cluster_num
+    RGB_vector(idx==i,1) = C(i,1);
+    RGB_vector(idx==i,2) = C(i,2);
+    RGB_vector(idx==i,3) = C(i,3);
+end;
+
+red_Q = reshape(RGB_vector(:,1), image_size(1), image_size(2));
+green_Q = reshape(RGB_vector(:,2), image_size(1), image_size(2));
+blue_Q = reshape(RGB_vector(:,2), image_size(1), image_size(2));
+
+image_Q = zeros(image_size);
+
+image_Q(:,:,1) = red_Q;
+image_Q(:,:,2) = green_Q;
+image_Q(:,:,3) = blue_Q;
+
+image_Q = uint8(image_Q);
+result = A - image_Q;
+
+figure(5);
+imshow(image_Q);
+str = sprintf('Quantised Image with %d k-means Clusters',cluster_num);
+title(str);
 
