69eb6636662088f00ed301ecad82cb1535f7632e
HarveyQ <qiuhuaqi@hotmail.com>
2016-03-01 17:18:26 +0000

    Haley's push
    sync Q1 for cw2

--- bow_svm/test1.m
+++ bow_svm/test1.m
@@ -1,7 +1,20 @@
 % Example
- data = load('pentagon');
- options = struct('ker','rbf','arg',1,'C',10);
- model = bsvm2(data,options);
- figure;
- ppatterns(data); ppatterns(model.sv.X,'ok',12);
- pboundary(model);
+clc;clear;close all
+
+load fisheriris
+inds = ~strcmp(species,'setosa');
+X = meas(inds,3:4);
+y = species(inds);
+
+figure
+scatter(X);
+
+SVMModel = fitcsvm(X,y);
+
+sv = SVMModel.SupportVectors;
+figure
+gscatter(X(:,1),X(:,2),y)
+hold on
+plot(sv(:,1),sv(:,2),'ko','MarkerSize',10)
+legend('versicolor','virginica','Support Vector')
+hold off
---
+++ bow_svm/test2.m
@@ -0,0 +1,11 @@
+% test 2
+% rng test
+
+clc;clear;close all;
+
+s = rng(1);
+
+x = rand(1,24);
+
+rng(s);
+
