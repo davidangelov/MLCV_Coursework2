b75321b5d63d8115f14d9b513949895a33861bf2
HarveyQ <qiuhuaqi@hotmail.com>
2016-03-01 15:08:19 +0000

    delete toolbox stpr tool

---
+++ bow_svm/CWQ2_HQ.m
@@ -0,0 +1,15 @@
+clc; clear; close all;
+
+% init;
+
+% Select and load dataset
+[data_train, data_test] = getData('Toy_Spiral');
+
+%% Isolate from data loading
+
+figure % plot training data
+plot_toydata(data_train);
+title('Training data');
+
+% figure % plot testing data
+% scatter(data_zxtest(:,1),data_test(:,2),'.b');
