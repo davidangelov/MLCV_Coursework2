4a388272ad11d3e3768f051549fa004862cdceeb
HarveyQ <qiuhuaqi@hotmail.com>
2016-03-18 13:00:27 +0000

    modified 1v1 & 1vR SVM
    modified the functions so that it takes the kernel coefficients as
    inputs

--- bow_svm/CW2Q3_HQ.m
+++ bow_svm/CW2Q3_HQ.m
@@ -2,15 +2,21 @@ clc; clear; close all;
 
 init;
 
+%% Data
+
 [data_train, data_test] = getData_HQ('Caltech', 100);
 
-%% 
-mode = 1; % 1 for 1vsRest, 2 for 1vs1
+%% SVM
+kernel = 'RBF';
+C = Inf;
+sigma = 400;
+mode = 'ovr';
+
 switch mode
-    case 1
-        predict_label = fMSVM_1vR(data_train, data_test);
-    case 2
-        predict_label = fMSVM_1v1(data_train, data_test);
+    case 'ovr'
+        predict_label = fMSVM_1vR(data_train, data_test, kernel, C, sigma);
+    case 'ovo'
+        predict_label = fMSVM_1v1(data_train, data_test, kernel, C, sigma);
 end
 
 correct_rate = length(find(predict_label == data_test(:,end)))/length(data_test(:,end));
@@ -18,18 +24,7 @@ display(correct_rate, 'Correction rate');
 
 %% Confusion matrix
 
+figure(1)
 conf_mat = confusionmat(predict_label, data_test(:,end));
 imagesc(conf_mat); colorbar;
 xlabel('Target class'), ylabel('Predicted class');
-
-% target = zeros(10,150);
-% output = zeros(10,150);
-% 
-% for i = 1:150
-%     
-%     target(data_test(i,end), i) = 1;
-%     output(predict_label(i,end), i) = 1;
-%     
-% end
-% 
-% plotconfusion(target, output);
--- bow_svm/fMSVM_1v1.m
+++ bow_svm/fMSVM_1v1.m
@@ -11,7 +11,7 @@
 %                 predicted label for each row of testing data
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function predict_label = fMSVM_1v1(data_train, data_test)
+function predict_label = fMSVM_1v1(data_train, data_test, kernel, C, sigma)
 
 data_class = data_train(:,end);
 
@@ -19,28 +19,44 @@ class_type = unique(data_train(:,end));
 M = length(class_type);
 %num_svm = M*(M-1)/2;
 vote = [];
-C = Inf;
-Kernel_Scale = 'auto';
+Kernel_Scale = sigma;
 
 for id_svm_i = 1 : M - 1
     for id_svm_j = id_svm_i + 1 : M
         data_train_svm = data_train((data_class == class_type(id_svm_i) | data_class == class_type(id_svm_j)), :);
         
+        switch kernel
+        case 'RBF'
         SVM = fitcsvm(data_train_svm(:,1:end-1), data_train_svm(:,end),...
-        'KernelFunction', 'rbf','BoxConstraint',C,'KernelScale',Kernel_Scale,...
-        'ClassNames',[id_svm_j,id_svm_i]);
+                    'KernelFunction', 'rbf',...
+                    'BoxConstraint',C,...
+                    'KernelScale',Kernel_Scale,...
+                    'ClassNames',[id_svm_j,id_svm_i]);
+    
+        case 'Poly'
+        SVM = fitcsvm(data_train_svm(:,1:end-1), data_train_svm(:,end),...
+                    'KernelFunction', 'polynomial',...
+                    'BoxConstraint',1,...
+                    'KernelScale', Kernel_Scale, ...
+                    'PolynomialOrder',C, ...
+                    'ClassNames',[id_svm_j,id_svm_i]);
+        end
         
         vote_svm = predict(SVM,data_test(:,1:end-1));
         vote = [vote, vote_svm];
+    
     end
 end
 
-[predict_label, vote_freq, values_with_freq] = mode(vote, 2);
+[predict_label, ~, values_with_freq] = mode(vote, 2);
 
-for i = 1:length(values_with_freq)
-    if length(cell2mat(values_with_freq(i,1))) ~= 1
-        predict_label(i,1) = 0;
+    % deal with fuzzy cases
+    for i = 1:length(values_with_freq)
+        if length(cell2mat(values_with_freq(i,1))) ~= 1
+            predict_label(i,1) = 0;
+        end
     end
-end
 
+display('1 vs 1 SVM training and testing completed');
+    
 end
