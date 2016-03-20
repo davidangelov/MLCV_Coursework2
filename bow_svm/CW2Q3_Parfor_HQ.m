e0655f6794c1d0d7b29fa07f5224f65074a671b2
HarveyQ <qiuhuaqi@hotmail.com>
2016-03-20 03:45:39 +0000

    par for HQ

---
+++ bow_svm/CW2Q3_Parfor_HQ.m
@@ -0,0 +1,46 @@
+% CW2_Q3_PARFOR
+clc; clear; close all;
+
+init;
+
+K = 50; % or whatever value you find is the best
+
+%% Set paramters
+C_range = 2.^(-20:5:20); % 9 of this
+sigma_range = 2.^(5:5:40); % 8 of this
+% 8*9*10*time_per_loop
+
+kernel = 'RBF';
+mode = 'ovr';
+
+%% loopssssss
+
+correct_rate_mean = zeros(length(C_range), length(sigma_range));
+
+for idx_C = 1:length(C_range)
+for idx_sigma = 1:length(sigma_range)
+    
+sigma = sigma_range(idx_sigma);
+C = C_range(idx_C);
+correct_rate = zeros(1,10);
+
+parfor iter = 1:10
+    
+[data_train, data_test] = getData_HQ('Caltech', K);
+
+switch mode
+    case 'ovr'
+        predict_label = fMSVM_1vR(data_train, data_test, kernel, C, sigma);
+    case 'ovo'
+        predict_label = fMSVM_1v1(data_train, data_test, kernel, C, sigma);
+end
+correct_rate(iter) = length(find(predict_label == data_test(:,end)))/length(data_test(:,end));
+
+end %iteration
+
+correct_rate_mean(idx_C, idx_sigma) = mean(correct_rate);
+
+end %sigma
+end %C
+
+
